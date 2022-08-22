f0f317ef40fa7f344b8b708f83de8ce5f377732c
Abdullah Talayhan <abdullahttj@gmail.com>
2022-08-22 20:15:22 +0200

    Update tag-header-widget.js

--- javascripts/discourse/widgets/tag-header-widget.js
+++ javascripts/discourse/widgets/tag-header-widget.js
@@ -22,6 +22,9 @@ export default createWidget("tag-header-widget", {
 
   html() {
     console.log(this.topic.tags)
+    this.store.find("tag-info", tag).then((result) => {
+      console.log(result)
+    });
     const router = getOwner(this).lookup("router:main");
     const route = router.currentRoute;
     const hideMobile =
